ef489aecd11d41ec77152d586e33efa9afb955e7
madhuri-anumula <madhu.ksmile@gmail.com>
2020-10-18 16:12:06 +0530

    initial commit

---
+++ precache-manifest.6ab32500604ca11fe37f990d18504751.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9552990a752e93a7c025aa833bb63152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c17e60db1f33d699c244",
+    "url": "/static/css/main.59ec94fe.chunk.css"
+  },
+  {
+    "revision": "016ac4463606b2748cad",
+    "url": "/static/js/2.bbd265d5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.bbd265d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17e60db1f33d699c244",
+    "url": "/static/js/main.7723de37.chunk.js"
+  },
+  {
+    "revision": "81d08e04a2e4215dd2ef",
+    "url": "/static/js/runtime-main.5856509f.js"
+  },
+  {
+    "revision": "efb62dd69c2f866d8393cd1113881257",
+    "url": "/static/media/books.efb62dd6.jpg"
+  }
+]);
